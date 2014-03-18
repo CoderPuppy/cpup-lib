96a834e72a5f18c87fe77502c04a28cd73eb131e
CoderPuppy <cpup@cpup-laptop>
2014-03-18 14:18:47 -0400

    Make checkNull just check for null and return a default

--- src/main/scala/cpup/lib/util/package.scala
+++ src/main/scala/cpup/lib/util/package.scala
@@ -1,11 +1,11 @@
 package cpup.lib
 
 object Util {
-	def checkNull[T, R](obj: T, fn: (T) => R, default: R): R = {
+	def checkNull[R](obj: R, default: R): R = {
 		if(obj == null) {
 			default
 		} else {
-			fn(obj)
+			obj
 		}
 	}
 }
