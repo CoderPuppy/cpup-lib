7607db60c8ba2fde0b82cc9420dbc6c2180e1de3
CoderPuppy <cpup@cpup-laptop>
2014-05-20 09:27:21 -0400

    Add Module.fullName, Returns the name with the parent's full name

--- src/main/scala/cpup/lib/module/Module.scala
+++ src/main/scala/cpup/lib/module/Module.scala
@@ -5,6 +5,7 @@ import org.slf4j.{Logger, LoggerFactory}
 
 trait Module[I] {
 	protected val _name = getClass.getSimpleName.replaceFirst("\\$$", "")
+	def fullName = parent.map(_.fullName + "/").getOrElse("") + name
 	def name: String = _name
 	def parent: Option[Module[_]] = None
 	def logger: Logger = parent.map((parent) => {
