43ce36ef4762b7f0d7aecae78b40d89945815a06
CoderPuppy <drewyoung1@gmail.com>
2015-06-12 21:51:14 -0400

    Make EventBus use ReflectUtil

--- src/main/scala/cpup/lib/events/EventBus.scala
+++ src/main/scala/cpup/lib/events/EventBus.scala
@@ -6,26 +6,34 @@ import scala.collection.mutable
 import scala.reflect.ClassTag
 import scala.reflect.runtime.{universe => ru}
 
+import cpup.lib.reflect.ReflectUtil
+
 trait EventBus {
 	private val handlers = new mutable.HashMap[ru.Type, mutable.Set[(ru.MethodMirror, Int)]]
 
-	def register[T](obj: T)(implicit tt: ru.TypeTag[T]) {
-		for((m, tpe, prior) <- EventBus.handlers[T](obj)) {
+	def register[T](obj: T, tpe: ru.Type) {
+		for((m, tpe, prior) <- EventBus.handlers[T](obj, tpe)) {
 			handlers.getOrElseUpdate(tpe, new mutable.HashSet[(ru.MethodMirror, Int)]) += ((m, prior))
 		}
 	}
 
-	def unregister[T](obj: T)(implicit tt: ru.TypeTag[T]) {
-		for((m, tpe, prior) <- EventBus.handlers[T](obj)) {
+	def register [T](obj: T)(implicit tt: ru.TypeTag[T]) { register[T](obj,          tt.tpe     ) }
+	def register_[T](obj: T)                             { register[T](obj, ReflectUtil.tpe(obj)) }
+
+	def unregister[T](obj: T, tpe: ru.Type) {
+		for((m, tpe, prior) <- EventBus.handlers[T](obj, tpe)) {
 			handlers.get(tpe).foreach(_ -= ((m, prior)))
 		}
 	}
 
-	def emit[T](e: T)(implicit tt: ru.TypeTag[T]) {
+	def unregister [T](obj: T)(implicit tt: ru.TypeTag[T]) { unregister[T](obj,          tt.tpe     ) }
+	def unregister_[T](obj: T)                             { unregister[T](obj, ReflectUtil.tpe(obj)) }
+
+	def emit[T](e: T, tpe: ru.Type) {
 		try {
-			for((handler, anno) <- handlers.filter(tt.tpe <:< _._1).flatMap(_._2).toList.sortBy(-_._2)) {
+			for((handler, anno) <- handlers.filter(tpe <:< _._1).flatMap(_._2).toList.sortBy(-_._2)) {
 				if(handler.symbol.paramLists.length == 2)
-					handler.apply(e, tt)
+					handler.apply(e, tpe)
 				else
 					handler.apply(e)
 			}
@@ -33,6 +41,14 @@ trait EventBus {
 			case StopEvent =>
 			case ex: InvocationTargetException if ex.getCause == StopEvent =>
 		}
+	}
+
+	def emit [T](obj: T)(implicit tt: ru.TypeTag[T]) { emit[T](obj,          tt.tpe     ) }
+	def emit_[T](obj: T)                             { emit[T](obj, ReflectUtil.tpe(obj)) }
+
+	@EventHandler
+	def forward(e: Any)(tpe: ru.Type) {
+		emit(e, tpe)
 	}
 }
 
@@ -43,20 +59,18 @@ object EventBus {
 		paramLists.length >= 1 && paramLists(0).length == 1 &&
 		(if(paramLists.length == 2) {
 			paramLists(1).length == 1 &&
-			paramLists(1)(0).typeSignature.typeConstructor == ru.typeOf[ru.TypeTag[_]].typeConstructor
+			paramLists(1)(0).typeSignature == ru.typeOf[ru.Type]
 		} else {
 			paramLists.length == 1
 		})
 	}
 
-	def handlers[T](implicit tt: ru.TypeTag[T]) = {
+	def handlers[T](tpe: ru.Type) = {
 		val ehTpe = ru.typeOf[EventHandler]
-		for {
-			claSym <- tt.tpe.baseClasses
-			tpe = claSym.info
-			decl <- tpe.decls if decl.isMethod
-			sym = decl.asMethod if validParams(sym.paramLists)
-			anno <- decl.annotations.find(_.tree.tpe == ehTpe)
+		(for {
+			member <- tpe.members if member.isMethod
+			sym = member.asMethod if validParams(sym.paramLists)
+			anno <- member.annotations.find(_.tree.tpe == ehTpe)
 			paramTpe = sym.paramLists.head.head.typeSignature
 		} yield (
 			sym, paramTpe,
@@ -68,14 +82,13 @@ object EventBus {
 				.flatMap { case n: Int => Some(n) case _ => None }
 				.find(c => true)
 				.getOrElse(0)
-		)
+		)).toList
 	}
 
-	def handlers[T](obj: T)(implicit tt: ru.TypeTag[T]): List[(ru.MethodMirror, ru.Type, Int)] = {
-		val tpe = tt.tpe
+	def handlers[T](obj: T, tpe: ru.Type): List[(ru.MethodMirror, ru.Type, Int)] = {
 		val m = ru.runtimeMirror(obj.getClass.getClassLoader)
 		implicit val ct = ClassTag[T](m.runtimeClass(tpe))
 		val im = m.reflect(obj)
-		handlers[T](tt).map(e => (im.reflectMethod(e._1), e._2, e._3))
+		handlers[T](tpe).map(e => (im.reflectMethod(e._1), e._2, e._3))
 	}
 }
